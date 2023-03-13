4027fd954ce6e45ddcfb3cf141f7a395e04350f0
brian-ikiara <brnikiara@gmail.com>
2023-03-13 16:10:55 +0300

    Update 0-fizzbuzz.py

--- 0-fizzbuzz.py
+++ 0-fizzbuzz.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-"""FizzBuzz module."""
+""" FizzBuzz
+"""
 import sys
 
 
 def fizzbuzz(n):
-    """Print numbers from 1 to n separated by a space.
+    """
+    FizzBuzz function prints numbers from 1 to n separated by a space.
 
-    For multiples of three print "Fizz" instead of the number and for
-    multiples of five print "Buzz".
-    For numbers which are multiples of both three and five print "FizzBuzz".
-
-    Args:
-        num (int): Number
-
+    - For multiples of three print "Fizz" instead of the number and for
+      multiples of five print "Buzz".
+    - For numbers which are multiples of both three and five print "FizzBuzz".
     """
     if n < 1:
         return
